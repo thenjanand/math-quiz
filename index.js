38bfa3d2c35ffceb02e5fc463a344a0158dd137d
NEERAJ ANAND <thenjanand@gmail.com>
2023-02-19 09:59:02 +0530

    html css work completed

---
+++ index.js
@@ -0,0 +1,8 @@
+// Create a Math Question
+// Math Question will have a random generated
+// Question Type Mujltiplication question with random number range 1-10
+// Answer will be the product of the random number range and the random number
+// User will have to answer question
+// On submit answer will be compared with random generated answer
+// If answer will be correct then score will be incremented
+// If answer will be wrong then score will be decremented
