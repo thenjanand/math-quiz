370b309cdd8ed804f28c63cb64a0ad9d991ca0e2
NEERAJ ANAND <thenjanand@gmail.com>
2023-02-21 23:51:14 +0530

    minor changes

--- index.js
+++ index.js
@@ -6,9 +6,38 @@
 // On submit answer will be compared with random generated answer
 // If answer will be correct then score will be incremented
 // If answer will be wrong then score will be decremented
+const questionEl = document.getElementById("question");
+const questionFormEl = document.getElementById("questionForm");
+let storedAnswer;
 
-const randomNumber = (min,max) => {
-return Math.floor(Math.random()*(max-min+1)+min);
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-console.log(randomNumber(1,10));
+console.log(randomNumber(1, 10));
+
+const genetrateQuestion = () => {
+  const randomNumber1 = randomNumber(1, 10);
+  const randomNumber2 = randomNumber(1, 10);
+
+  const question = `Q. What is ${randomNumber1} multiply by ${randomNumber2} ?`;
+  const answer = randomNumber1 * randomNumber2;
+  return { question, answer };
+};
+
+console.log(genetrateQuestion());
+
+const showQuestion = () => {
+  const { question, answer } = genetrateQuestion();
+  questionEl.innerHTML = question;
+  storedAnswer = answer;
+};
+
+showQuestion();
+
+const checkAnswer = (e) => {
+  e.preventDefault();
+  const formData = new FormData(questionFormEl);
+
+  console.log(formData);
+};
